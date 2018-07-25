e4fe0c07db9ba2508f66192975e1f31303cd58a7
nooha mohammed <noohamohammed00@gmail.com>
2018-07-25 20:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+const logDriverNames = function(driver){
+    driverInfo.forEach(function(driver){
+       console.log(driver.name)
+    })
+}
+
