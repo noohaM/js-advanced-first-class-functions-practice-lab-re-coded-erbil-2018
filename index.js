de51a55936c30e73bc370c443917a0d4a90fab48
nooha mohammed <noohamohammed00@gmail.com>
2018-07-25 20:18:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 // Code your solution in this file!
-// Code your solution in this file!
 
-const logDriverNames = function(driver){
+const logDriverNames = function(driverInfo){
     driverInfo.forEach(function(driver){
        console.log(driver.name)
     })
 }
+
+const logDriversByHometown = function(driverInfo, hometown){
+  driverInfo.forEach(function(driver){
+    if (driver.hometown === hometown){
+      console.log(driver.name)
+    }
+  })
+}
+
+const driversByRevenue = function(driver){
+  const newArray = []
+  driver.forEach(function(driver){
+    newArray.push(driver)
+  })
+  newArray.sort(function(a,b){
+    return a.revenue - b.revenue
+  })
+  return newArray
+}
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
+
 
